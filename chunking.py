4f06a5cbcaf5ae1a186c7bd0ea60c36b9fdd4de0
DucLV <luuvanductn1999@gmail.com>
2025-07-27 12:31:25 +0700

    change path

--- chunking.py
+++ chunking.py
@@ -1134,5 +1134,5 @@ if __name__ == '__main__':
       path=file,
       title=file.name,
     )
-    with open(f'./data/{file.stem}.json', 'w', encoding='utf-8') as f:
+    with open(f'./chunk/{file.stem}.json', 'w', encoding='utf-8') as f:
       f.write(json.dumps(s.chunks, ensure_ascii=False, indent=2))
